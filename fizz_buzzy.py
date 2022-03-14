05eb96568a08365eb04dc16c85b4c4e747f28854
Joseph Kowalski <jkjr25@gmail.com>
2022-03-14 17:04:46 -0400

    Completed FizzBuzz exercise

---
+++ fizz_buzzy.py
@@ -0,0 +1,12 @@
+for fizzbuzz in range(101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print('FizzBuzz')
+        continue
+    elif fizzbuzz % 3 == 0:
+        print('Fizz')
+        continue
+    elif fizzbuzz % 5 == 0:
+        print('Buzz')
+        continue
+    else:
+        print(fizzbuzz)
